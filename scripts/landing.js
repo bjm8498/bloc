5beefccf15960f5d67f5ad175cd1ac32cfbb1502
Brian Moore <bjm8498@gmail.com>
2017-01-30 03:17:02 -0800

    Finished checkpoint 9 DOM scripting events.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,31 @@
-var revealPoint = function(){
-                
-    var points = document.getElementsByClassName('point')
-    
-    for(var i = 0; i < points.length; i++){
-        
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebKitTransform = "scaleX(1) translateY(0)";
+var pointsArray = document.getElementsByClassName('point');
 
-    }
-                
-    
+
+
+
+var revealPoint = function (point) {
+
+        
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(0)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebKitTransform = "scaleX(1) translateY(0)";
 
 };
+
+var animatePoints = function(points){
+    forEach(points, revealPoint);
+}
+
+window.onload = function(){
+    
+    if(window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    window.addEventListener('scroll',function(event){
+      if (pointsArray[0].getBoundingClientRect().top <= 500) {
+          animatePoints(pointsArray);
+      }    
+    });
+}
