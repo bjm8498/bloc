24c36e605e26540f73eaf97619f97863e12d364f
Brian Moore <bjm8498@gmail.com>
2017-02-06 15:15:28 -0800

    It works

--- scripts/album.js
+++ scripts/album.js
@@ -46,7 +46,7 @@ var albumMe = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template = 
         '<tr class = "album-view-song-item">'
-    + '<td class = "song-item-number">' + songNumber + '</td>'
+    + '<td class = "song-item-number" >' + songNumber + '</td>'
     + '<td class = "song-item-title">' + songName + '</td>'
     + '<td class = "song-item-duration">' + songLength + '</td>'
     + '</tr>'
@@ -77,11 +77,18 @@ var setCurrentAlbum = function (album) {
     }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0]
 
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+var playButtonTemplate = '<a class== "album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
     
+    
+
     var albumarray = [albumPicasso, albumMarconi, albumMe];
     var index = 1;
     albumImage.addEventListener('click', function (event) {
@@ -91,6 +98,22 @@ window.onload = function() {
             index = 0;
         }
     })
+    
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+          // Change the content from the number to the play button's HTML//
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+    
+        for(var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseLeave', function(event) {
+            
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            
+        });
+    }
+    
 };
 
 
